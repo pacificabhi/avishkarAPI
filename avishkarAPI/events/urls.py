3fa72c8368b9e8d7ba798a8f52076fb60f80abbc
Abhishek Kumar <abhishekad7@gmail.com>
2020-08-15 22:11:31 +0530

    create team added

--- avishkarAPI/events/urls.py
+++ avishkarAPI/events/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .import views
 
 urlpatterns = [
-    
+    path('createteam/', views.CreateTeam.as_view(), name='createteam'),
 ]
--- avishkarAPI/events/views.py
+++ avishkarAPI/events/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import *
+
+class CreateTeam(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
+
+    def post(self, request):
+        u = request.user
+
+        if u.is_staff:
+            context = {"success": False, "errors": ["You are an event staff member, You can not create teams"]}
+            return Response(context)
+
+        team_name = request.POST.get("teamname").strip()
+        team = EventTeam.objects.create(team_name=team_name, team_admin=u)
+
+        team_id = "TEAM" + str(team.pk)
+        team.team_id = team_id
+
+        team.add_team_member(u)
+        team.save()
+
+        context = {"success": True, "team_id": team_id}
+
+        return Response(context)
+
+
